4aacab1b0d01b64b7fb1382e83e63c6a7536cb27
Arnaud Héritier <aheritier@apache.org>
2012-01-09 22:04:40 +0100

    JENKINS-12217 : fails to set default keychain when using alternate (non login) keychain

--- src/main/java/au/com/rayh/XCodeBuilder.java
+++ src/main/java/au/com/rayh/XCodeBuilder.java
@@ -311,7 +311,7 @@ public class XCodeBuilder extends Builder {
         if (unlockKeychain) {
             // Let's unlock the keychain
             launcher.launch().envs(envs).cmds("/usr/bin/security", "list-keychains", "-s", keychainPath).stdout(listener).pwd(projectRoot).join();
-            launcher.launch().envs(envs).cmds("/usr/bin/security", "login-keychain", "-d", "user", "-s", keychainPath).stdout(listener).pwd(projectRoot).join();
+            launcher.launch().envs(envs).cmds("/usr/bin/security", "default-keychain", "-d", "user", "-s", keychainPath).stdout(listener).pwd(projectRoot).join();
             if (StringUtils.isEmpty(keychainPwd))
                 returnCode = launcher.launch().envs(envs).cmds("/usr/bin/security", "unlock-keychain", keychainPath).stdout(listener).pwd(projectRoot).join();
             else
